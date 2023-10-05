b4bb73143eac30e0714026eb962d2b65081871d8
p-mohan <>
2023-10-06 02:40:30 +0530

    path fix

--- django_notes_app/settings.py
+++ django_notes_app/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = env("SECRET_KEY")
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 BACKEND_DIR = BASE_DIR  # rename variable for clarity
-FRONTEND_DIR = BASE_DIR.parent / 'react-notes-app'
+FRONTEND_DIR = BASE_DIR / 'react-notes-app'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
