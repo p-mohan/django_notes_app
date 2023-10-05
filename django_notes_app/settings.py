ea13fe2f6ee999439569f5ef62f07ea099f8a434
p-mohan <>
2023-10-06 02:48:57 +0530

    mod

--- django_notes_app/settings.py
+++ django_notes_app/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [FRONTEND_DIR / 'build' / 'static']
-STATIC_ROOT = BACKEND_DIR  /"staticfiles_build"/ 'static'
+STATIC_ROOT = BACKEND_DIR  / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
